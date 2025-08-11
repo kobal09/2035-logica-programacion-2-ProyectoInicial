9c31175fd481a51ce783634d23bd5e152d01cc21
kobal09 <kobal0901@gmail.com>
2025-08-11 15:15:44 -0500

    crear proyecto de ONEG09 - logica de programacion

---
+++ app.js
@@ -0,0 +1,10 @@
+let $_titulo = document.querySelector('h1'),
+    $_parrafo = document.querySelector('p');
+
+
+$_titulo.innerHTML = "Juego del Numero Secreto";
+$_parrafo.innerHTML = "Indica un numero del 1 al 10";
+
+function intentoDeUsuario(){
+    
+}
