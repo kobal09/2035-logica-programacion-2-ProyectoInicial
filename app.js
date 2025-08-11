d169fafd35b8cda476f6328c2824194e0fe2f9f2
kobal09 <kobal0901@gmail.com>
2025-08-11 15:31:30 -0500

    crear funcion asignarTextoElemento, para automatizar el codigo

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-let $_titulo = document.querySelector('h1'),
+/* let $_titulo = document.querySelector('h1'),
     $_parrafo = document.querySelector('p');
 
 
 $_titulo.innerHTML = "Juego del Numero Secreto";
-$_parrafo.innerHTML = "Indica un numero del 1 al 10";
+$_parrafo.innerHTML = "Indica un numero del 1 al 10"; */
+
+//se creo una funcion para no repetir el automatizar la interaccion con el html
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
 
 function intentoDeUsuario(){
-    
+
 }
+
+asignarTextoElemento('h1', 'Juego del Numero Secreto!');
+asignarTextoElemento('p', 'Indica un numero del 1 al 10!');
